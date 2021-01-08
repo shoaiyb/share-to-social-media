85dbea9e94880e014bb6159ca18a89b3adfae4cc
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-01-08 23:09:37 +0100

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,6 @@
 function hide_show_tr_select_animation(){
-	var radio_ss_style = jQuery('input:radio[name="s3_options[ss-select-style]"]');
-	if ( radio_ss_style.is(':checked') && ( radio_ss_style.filter(':checked').val() == 'horizontal-with-count' || radio_ss_style.filter(':checked').val() == 'small-buttons' ) ) {
+	var radio_stsm_style = jQuery('input:radio[name="stsm_options[stsm-select-style]"]');
+	if ( radio_stsm_style.is(':checked') && ( radio_stsm_style.filter(':checked').val() == 'horizontal-with-count' || radio_stsm_style.filter(':checked').val() == 'small-buttons' ) ) {
         jQuery("tr.tr-select-animation").hide();
     }
     else{
@@ -11,7 +11,7 @@ function hide_show_tr_select_animation(){
 jQuery(document).ready(function(){
 
 	hide_show_tr_select_animation();
-	jQuery('input:radio[name="s3_options[ss-select-style]"]').change(function(){
+	jQuery('input:radio[name="stsm_options[stsm-select-style]"]').change(function(){
         /*if ( $(this).is(':checked') && ( $(this).val() == 'horizontal-with-count' || $(this).val() == 'small-buttons' ) ) {
 	        jQuery("tr.tr-select-animation").hide();
 	    }
