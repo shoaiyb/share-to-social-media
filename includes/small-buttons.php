85c6ad9868a5f87d88634a63d2bb22d1d1ac3d57
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-01-22 23:44:59 +0100

    Update small-buttons.php

--- includes/small-buttons.php
+++ includes/small-buttons.php
@@ -15,7 +15,7 @@ function get_small_buttons_markup_arr( $stsm_options ){
 												</div>',
 								'digg' => '',
 								'reddit' => '<div class="s-single-share">
-												<a href="http://www.reddit.com/submit" onclick="window.location = \'http://www.reddit.com/submit?url=\' + encodeURIComponent(window.location); return false"> <img src="http://www.reddit.com/static/spreddit7.gif" alt="submit to reddit" border="0" /> </a>
+												<a href="http://www.reddit.com/submit" onclick="window.location = \'http://www.reddit.com/submit?url=\' + encodeURIComponent(window.location); return false"> <img src="'.plugins_url( '../images/spreddit7.gif' , __FILE__ ).'"></a>
 											</div>',
 								'linkedin' => '<div class="s-single-share">
 													<script type="IN/Share"></script>
@@ -27,7 +27,7 @@ function get_small_buttons_markup_arr( $stsm_options ){
 												<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:61px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_2.png\') top left no-repeat transparent;">Share on Tumblr</a>
 												</div>',
 								'pinterest' => '<div class="s-single-share">
-													<a href="//www.pinterest.com/pin/create/button/" data-pin-do="buttonBookmark"  data-pin-color="red"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_20.png" /></a>
+													<a href="//www.pinterest.com/pin/create/button/" data-pin-do="buttonBookmark"  data-pin-color="red"><img src="'.plugins_url( '../images/pinit_fg_en_rect_red_20.png' , __FILE__ ).'"></a>
 													<script type="text/javascript" async defer src="//assets.pinterest.com/js/pinit.js"></script>
 												</div>',
 								'email' => '<div class="s-single-share">
